a4d8c92e8b45b8ed174fce92ecd30dc78252e383
JesseCochran <jcochran@pietech.com>
2017-06-28 09:51:36 -0400

    Added readFile class and added csv file with life expectancy data.

---
+++ life-expectancy-calculator/src/utilities/readFile.js
@@ -0,0 +1,23 @@
+import { HttpClient } from 'aurelia-fetch-client';
+
+export class ReadFile {
+    constructor() {
+
+    }
+
+    //Given a state as input this method reads the csv file and returns all counties for that state.
+    getCountyList(state) {
+        let httpClient = new HttpClient();
+
+        httpClient.fetch('IHME_USA_EXPECTANCY_1985_2010.csv')
+                .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            });
+    }
+
+    //Given a county name this method will return the life expectancy for that county
+    getCountyLifeExpectancy(county) {
+
+    }
+}
