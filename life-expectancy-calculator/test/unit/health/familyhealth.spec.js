619cbe2984fa51c4e15617d7b7ae52b89efa0e8f
JesseCochran <jcochran@pietech.com>
2017-07-06 11:17:27 -0400

    Refactored code, images, and folder directories.

---
+++ life-expectancy-calculator/test/unit/health/familyhealth.spec.js
@@ -0,0 +1,12 @@
+import {CalculateMyHealth} from '../../../src/utilities/calculations/calculateMyHealth';
+import {User} from '../../../src/services/user';
+
+describe('family health', () => {
+
+    //Set up client data for testing
+    beforeEach(() => {
+
+    })
+
+    //TODO: Enter test for impact of family health on life expectancy
+})
