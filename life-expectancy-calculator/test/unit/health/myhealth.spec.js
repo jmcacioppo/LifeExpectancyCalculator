952756c4832b920bb00af298b583c6490c0eb0aa
JesseCochran <jcochran@pietech.com>
2017-07-06 10:30:53 -0400

    Added comments to test. Added test file for occupation.

--- life-expectancy-calculator/test/unit/health/myhealth.spec.js
+++ life-expectancy-calculator/test/unit/health/myhealth.spec.js
@@ -1,32 +1,38 @@
 import {CalculateMyHealth} from '../../../src/utilities/calculateMyHealth';
 import {User} from '../../../src/services/user';
 
+//Test the myhealth page
 describe('my health', () => {
     var user = new User();
     var health = new CalculateMyHealth();
+    
+    //Sets up the client data before each test
     beforeEach(() => {
         user.clientMyHealth.height = "6'0";
         user.clientMyHealth.heightInInches = 72;
         user.clientMyHealth.weight = 165;
     });
 
+    //Test client height
     it('client height should be 72', () => {
         console.log(user.clientMyHealth.weight);
         expect(user.clientMyHealth.height).toBe("6'0");
     });
 
+    //Test client weight
     it('client weight should be 165', () => {
         expect(user.clientMyHealth.weight).toBe(165);
     });
 
+    //Test client bmi
     it('client bmi should be 22.92', () => {
         health.calculateBMI(user.clientMyHealth);
         expect(user.clientMyHealth.bmi).toBe('22.92');
     })
 
+    //Test changing client bmi
     it('changing the bmi value should change the clients bmi', () => {
         user.clientMyHealth.bmi = '232.23';
         expect(user.clientMyHealth.bmi).toBe('232.23');
     })
-
 });
---
+++ life-expectancy-calculator/test/unit/occupation/occupation.spec.js
@@ -0,0 +1,12 @@
+import {Occupation} from '../../../src/occupation/occupation';
+import {User} from '../../../src/services/user';
+
+//Test the occupation page
+describe('occupation', () => {
+    var user = new User();
+    beforeEach(() => {
+        //Enter data to set up user here
+    });
+
+    //TODO: Enter test for occupation impact on life expectancy
+});
