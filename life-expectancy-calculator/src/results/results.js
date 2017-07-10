a0fd8ce4bfe543b1c2ffa4f9d7b55a8249ca1c51
jmcacioppo <jcacioppo@PIE_MAIN.pietech.com>
2017-07-10 12:32:20 -0400

    Implementing some calculations based on if spouse dies early

--- life-expectancy-calculator/src/results/results.js
+++ life-expectancy-calculator/src/results/results.js
@@ -3,21 +3,29 @@ import {Router} from 'aurelia-router';
 import {User} from '../services/user';
 import {Chart} from '../utilities/chart';
 import {CalculateResults} from '../utilities/calculations/calculateResults';
+import {Slider} from '../utilities/slider';
 
-@inject(Router, User, Chart, CalculateResults)
+@inject(Router, User, Chart, CalculateResults, Slider)
 export class results {
-    constructor(router, user, chart, calculateResults) {
+    constructor(router, user, chart, calculateResults, slider) {
         this.router = router;
         this.user = user;
         this.chart = chart;
         this.calculateResults = calculateResults;
+        this.slider = slider;
     }
 
     checkSpouseDeath(person) {
-        if(person.checkSpouseDeath) console.log("Dying");
+        person.checkSpouseDeath = !person.checkSpouseDeath;
+    }
+
+    spouseDies(person) {
+        console.log(person.spouseDeath);
     }
 
     attached() {
+        this.slider.createSpouseDiesSlider();
+        
         var chartTuples = [];
         if(this.user.clientPersonalInfo.checkspouse) {
             chartTuples = [{
